6017e005efb939486ec9eeefd124ac8febe67518
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-21 17:14:08 +0100

    updated movement, somehow made jumping not work

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -2,86 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public enum SIDE { Left, Mid, Right}
 
 public class AgentController : MonoBehaviour
 {
     Rigidbody rb;
+    CharacterController cc;
 
     private SIDE m_side;
-    private float newXPos = 0f;
-    private float XValue = 6f;
+    public float newXPos = 0f;
+    public float XValue = 6f;
 
-    public float speed = 2f;
-    bool isGrounded = true;
+    public float speed = 5f;
+    public float jumpHeight = 10f;
+    private Vector3 playerVelocity;
 
     private Vector3 startingPos = new Vector3(0f, 1f, -96f);
+    private float gravity = -9.81f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Physics.gravity *= 2;
+        cc = GetComponent<CharacterController>();
         m_side = SIDE.Mid;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
-
-        if (Input.GetKey(KeyCode.W) && isGrounded)
-        {
-            Debug.Log("jump");
-            rb.AddForce(Vector3.up * 11, ForceMode.Impulse);
-            isGrounded = false;
-        }
-
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             if(m_side == SIDE.Mid)
             {
                 Debug.Log("from MID to LEFT");
-                newXPos = -XValue;
+                newXPos -= XValue;
                 m_side = SIDE.Left;
-                transform.Translate(new Vector3(newXPos, 0, 0));
             }
             else if(m_side == SIDE.Right)
             {
                 Debug.Log("from RIGHT to MID");
                 newXPos = 0;
                 m_side = SIDE.Mid;
-                transform.Translate(new Vector3(newXPos, 0, 0));
             }
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             if(m_side == SIDE.Mid)
             {
                 Debug.Log("from MID to RIGHT");
-                newXPos = XValue;
+                newXPos += XValue;
                 m_side = SIDE.Right;
-                transform.Translate(new Vector3(newXPos, 0, 0));
             }
             else if(m_side == SIDE.Left)
             {
                 Debug.Log("from LEFT to MID");
                 newXPos = 0;
                 m_side = SIDE.Mid;
-                transform.Translate(new Vector3(newXPos, 0, 0));
             }
         }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Shrub") || collision.gameObject.CompareTag("Log") || collision.gameObject.CompareTag("Tree") || collision.gameObject.CompareTag("Branch") || collision.gameObject.CompareTag("Sanic"))
+        Vector3 moveDir = new Vector3(newXPos - transform.position.x, 0, speed * Time.deltaTime);
+        cc.Move(moveDir);
+
+        if (Input.GetKeyDown(KeyCode.W) && cc.isGrounded)
         {
-            Debug.Log("Hit an obstacle!");
-            speed = 0f;
-            isGrounded = false;
+            Debug.Log("jump");
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
         }
-        else if(collision.gameObject.CompareTag("Ground")){
-            isGrounded = true;
-        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        cc.Move(playerVelocity * Time.deltaTime);
     }
 }
