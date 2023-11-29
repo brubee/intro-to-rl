9728469d163dee6e4d16911d130fa1009c35eeb1
Jurás Dorka <ijustcantjudo@gmail.com>
2023-11-29 22:42:00 +0100

    finished training, updated the assets, only need agent animation

--- Assets/Scripts/MyAgent.cs
+++ Assets/Scripts/MyAgent.cs
@@ -15,13 +15,12 @@ public class MyAgent : Agent
 
     private Vector3 startingPos = new Vector3(0f, 1f, -96f);
 
-    private float boundXLeft = -6f;
-    private float boundXRight = 6f;
+    private float boundXLeft = -6.5f;
+    private float boundXRight = 6.5f;
     private float boundY = 2f;
 
-    private int episodeCounter = 0;
-
-    public GameObject movingEnemy;
+    public int episodeCounter = 0;
+    private bool secondRound = false;
 
     private enum ACTIONS
     {
@@ -38,6 +37,11 @@ public class MyAgent : Agent
     private void FixedUpdate()
     {
         transform.Translate(Vector3.forward * m_speed * Time.fixedDeltaTime);
+
+        if (transform.position.y <= -2f)
+        {
+            EndEpisode();
+        }
     }
 
     public override void OnEpisodeBegin()
@@ -45,12 +49,32 @@ public class MyAgent : Agent
         transform.localPosition = startingPos;
         isGrounded = true;
         episodeCounter++;
-        movingEnemy.GetComponent<MovingEnemyController>().enabled = true;
+
+        if(episodeCounter >= 200)
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+
+        if(SceneManager.GetActiveScene().name == "Scene2" && episodeCounter >= 100)
+        {
+            SceneManager.LoadScene("Scene1");
+            secondRound = true;
+        }
+
+        if(SceneManager.GetActiveScene().name == "Scene1" && secondRound == true && episodeCounter >= 50)
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+
+        if(SceneManager.GetActiveScene().name == "Scene2" && secondRound == true && episodeCounter >= 50)
+        {
+            SceneManager.LoadScene("Scene1");
+        }
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // this should be done by the RayPerceptionSensor, but if it doesn't work how I want it to, I'll come back to it :')
+        // done with RayPerceptionSensor
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -90,19 +114,17 @@ public class MyAgent : Agent
             case (int)ACTIONS.LEFT:
                 if (transform.localPosition.x >= boundXLeft)
                     transform.Translate(-Vector3.right * a_speed * Time.fixedDeltaTime);
-                AddReward(0.01f);
                 break;
             case (int)ACTIONS.RIGHT:
                 if (transform.localPosition.x <= boundXRight)
                     transform.Translate(Vector3.right * a_speed * Time.fixedDeltaTime);
-                AddReward(0.01f);
                 break;
             case (int)ACTIONS.JUMP:
                 if (isGrounded && transform.localPosition.y < boundY)
                 {
                     rb.AddForce(Vector3.up, ForceMode.Impulse);
                 }
-                AddReward(-0.001f);
+                AddReward(-0.005f);
                 break;
         }
 
@@ -114,7 +136,6 @@ public class MyAgent : Agent
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             AddReward(-1.0f);
-            movingEnemy.GetComponent<MovingEnemyController>().enabled = false;
             EndEpisode();
         }
     }
@@ -124,8 +145,6 @@ public class MyAgent : Agent
         if (other.tag == "Goal")
         {
             AddReward(10.0f);
-            // and then I also want to create at least one more different level, which would be loaded in from here, once the agent gets to the goal
-            movingEnemy.GetComponent<MovingEnemyController>().enabled = false;
             EndEpisode();
         }
     }
